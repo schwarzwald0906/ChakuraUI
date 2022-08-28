8a8d6f09f550d005296ec49d0bb90dec06cb7484
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-29 08:46:20 +0900

    ログインページを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
-import { theme } from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>ああああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
