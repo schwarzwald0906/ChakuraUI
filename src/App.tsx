13033eb5a8d3d201f582b555e8aaa8c5aca6bc3b
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-28 22:15:55 +0900

    Globalなスタイルの適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
+import { theme } from "./theme/theme";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>ああああ</p>
+    </ChakraProvider>
   );
 }
